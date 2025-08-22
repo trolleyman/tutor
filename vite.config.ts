24fdec14fd68fd933efc40608a46a8dba595fd3b
Callum Tolley <cgtrolley@gmail.com>
2025-08-22 21:46:19 +0100

    Update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://trolleyman.github.io/tutor/", // TODO: Remove when deploying to custom domain
   plugins: [
     react(),
     tailwindcss(),
